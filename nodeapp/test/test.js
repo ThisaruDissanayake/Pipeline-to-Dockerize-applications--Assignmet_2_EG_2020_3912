9981d8da8b337f405e9cd509dd02b5faf3c73848
ThisaruDissanayake <thisarumitd@gmail.com>
2024-04-22 22:29:57 +0530

    last

--- nodeapp/test/test.js
+++
@@ -1,7 +0,0 @@
-var request = require('supertest');
-var app = require('../index.js');
-describe('GET /will', function() {
-    it('respond with Hello Welcome to Devops 3912', function(done) {
-        request(app).get('/will').expect('{ "response":"Hello Welcome to Devops 3912" }', done);
-    });
-});
