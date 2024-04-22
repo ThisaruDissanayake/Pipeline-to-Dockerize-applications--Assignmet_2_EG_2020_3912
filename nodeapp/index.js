826b0b56de27bae0a0158da199b20ae2aaa0446e
ThisaruDissanayake <thisarumitd@gmail.com>
2024-04-22 20:33:02 +0530

    second commit

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,14 +2,14 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "Hello Welcome to Devops Assingment EG/2020/3912" }');
+  res.send('{  "response":"Hello Welcome to Devops Assingment EG/2020/3912" }');
 });
 
 app.get("/will", function (req, res) {
-  res.send('{ "Hello Welcome to Devops 3912" }');
+  res.send('{  "response":"Hello Welcome to Devops 3912" }');
 });
 app.get("/ready", function (req, res) {
-  res.send('{ "EG/2020/3912 Wow!, It works!" }');
+  res.send('{  "response":"EG/2020/3912 Wow!, It works!" }');
 });
 
 //listen to port 3000 by default
